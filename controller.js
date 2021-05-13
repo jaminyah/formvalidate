93cd37189f9c1ae3c9482e2e843dccc1bfcc107a
jaminyah <jaminyah@gmail.com>
2021-05-13 13:43:26 -0400

    Raise passwd confirm error if base passwd changes.

--- controller.js
+++ controller.js
@@ -1,3 +1,22 @@
+
+// Reference: https://flaviocopes.com/how-to-validate-email-address-javascript/
+export const emailCheck = (text, emailError) => {
+    //let condition = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    let condition = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i
+
+    emailError.style.color = "red";
+
+    if (!text.match(condition)) {
+        emailError.style.display = "block";
+        emailError.innerText = "Invalid Email Address";
+        return;
+    }
+
+    emailError.style.display = "none";
+    return;
+}
+
+
 export const pswdCheck = (text, passwordError) => {
     let condition1 = /(?=.*\d)/; //should contain atleast 1 digit
     let condition2 = /(?=.*[a-z])/; //should contain atleast 1 lowercase
@@ -34,21 +53,19 @@ export const pswdCheck = (text, passwordError) => {
     return;
 }
 
-// Reference: https://flaviocopes.com/how-to-validate-email-address-javascript/
-export const emailCheck = (text, emailError) => {
-    //let condition = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    let condition = /(?!.*\.{2})^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.?$/i
 
-    emailError.style.color = "red";
 
-    if (!text.match(condition)) {
-        emailError.style.display = "block";
-        emailError.innerText = "Invalid Email Address";
+export const confirmPasswordCheck = (text, initPasswrd, confPasswordError) => {
+
+    confPasswordError.style.color = "red";
+
+    if (text !== initPasswrd) {
+        confPasswordError.style.display = "block";
+        confPasswordError.innerText = "Passwords don't match";
         return;
     }
 
-    emailError.style.display = "none";
+    confPasswordError.style.display = "none";
     return;
 }
 
-  
--- script.js
+++ script.js
@@ -1,13 +1,4 @@
-//import { pswdCheck } from './controller.js';
-import { emailCheck, pswdCheck } from './controller.js';
-
-let password = document.getElementById("pswd");
-let passwordError = document.getElementById("pswdError");
-passwordError.style.display = "none";
-password.addEventListener('keypress', event => {
-    let text = password.value + `${event.key}`;
-    pswdCheck(text, passwordError);
-});
+import { emailCheck, pswdCheck, confirmPasswordCheck } from './controller.js';
 
 let email = document.getElementById("email");
 let emailError = document.getElementById("emailError");
@@ -16,7 +7,49 @@ emailError.style.display = "none";
 email.addEventListener("keyup", event => {
 
     console.log('verify email')
-    //let text = email.value + `${event.key}`;
+
     let text = email.value
     emailCheck(text, emailError);
 });
+
+// Check password criteria
+let password = document.getElementById("pswd");
+let passwordError = document.getElementById("pswdError");
+
+passwordError.style.display = "none";
+password.addEventListener('keyup', event => {
+    let text = password.value + `${event.key}`;
+    pswdCheck(text, passwordError);
+});
+
+
+$(document).ready(function () {
+    //$("#txtUsername").val("");
+    //$("#txtNewPassword, #txtConfirmPassword").keyup(checkPasswordMatch);
+    $("#pswd, #confirmPswd").keyup(checkPasswordMatch);
+ });
+
+ 
+function checkPasswordMatch() {
+    
+    let password = $("#pswd").val();
+    let confirmPassword = $("#confirmPswd").val();
+
+    if (password != confirmPassword)
+        $("#confirmPswdError").html("Passwords do not match!");
+    else
+        $("#confirmPswdError").html("Passwords match.");
+} 
+
+
+/*
+let confPassword = document.getElementById("confirmPswd");
+let confPasswordError = document.getElementById("confirmPswdError");
+confPasswordError.style.display = "none";
+
+confPassword.addEventListener('keyup', event => {
+    let text = confPassword.value + `${event.key}`;
+    let initPasswrd = password.value;
+    confirmPasswordCheck(text, initPasswrd, confPasswordError);
+})
+*/
